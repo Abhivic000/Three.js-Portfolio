5a045ec43390a4019a18beafd6f8ddfc4c6190cc
Abhivic <1032210906@tcetmumbai.in>
2023-04-15 15:04:49 +0530

    commit

--- main.js
+++ main.js
@@ -49,7 +49,7 @@ function addStar() {
 
 Array(200).fill().forEach(addStar)
 
-const spaceTexture = new THREE.TextureLoader().load('space4k.jpg');
+const spaceTexture = new THREE.TextureLoader().load('new1.jpg');
 scene.background = spaceTexture;
 
 //Avatar
